9d9eb98067806730eea6e33f3db5ec79b45840ef
Eric Miller <eric@legoaces.org>
2015-11-30 23:09:04 -0500

    Progress

--- plot_resistances.m
+++ plot_resistances.m
@@ -2,4 +2,10 @@ Resistances = [4e3 20e3 60e3 249e3];
 Currents = 1.976 ./ Resistances;
 Measurements = [3.748 2.744 2.538 2.506];
 
-plot(Currents, Measurements, 'o');
+clf
+plot(Currents, Measurements, 'o-b');
+xlabel('Current (A)')
+ylabel('Measured voltage (V)')
+hold on
+pause
+plot(1.976/604, 4.96, 'ob');
